c06b6310373714454dbaa424bc55ce0c03f148f4
Ludvig Sundstrom <lud.sund@gmail.com>
2022-12-23 08:15:58 +0100

    Check for arguments

--- run_filewatch.zsh
+++ run_filewatch.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+if [ "$#" -ne 1 ]; then
+    echo "Missing argument: executable name"
+fi
+
 prog=$1
 
 stack install --file-watch --exec "killall $1"
--- run_server.zsh
+++ run_server.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+if [ "$#" -ne 1 ]; then
+    echo "Missing argument: executable path"
+fi
+
 stack_executable=$1
 
 while true; do;
